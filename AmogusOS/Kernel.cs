e13dacfff5364d362bee1077361a7f60d2812c3f
abjdev <91409956+abjdev@users.noreply.github.com>
2022-02-26 22:19:35 +0300

    Add files via upload

---
+++ AmogusOS/Kernel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+using Cosmos.System.Graphics;
+using System.IO;
+using System.Drawing;
+using Cosmos.Core;
+using AmogusOS.System;
+using AmogusOS.System.Converting;
+
+namespace AmogusOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            FS fs = new FS();
+            fs.InitFS();
+            Setup stp = new Setup();
+            stp.CheckForInstalled();
+        }
+
+        protected override void Run()
+        {
+
+        }
+    }
+}
