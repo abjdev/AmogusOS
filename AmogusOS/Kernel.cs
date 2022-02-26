d4f36f258fa1a057dd7fa9a208d464886f9b2589
abjdev <91409956+abjdev@users.noreply.github.com>
2022-02-26 22:36:30 +0300

    Add files via upload

--- AmogusOS/Kernel.cs
+++ AmogusOS/Kernel.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
@@ -27,3 +28,34 @@ namespace AmogusOS
         }
     }
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+using Cosmos.System.Graphics;
+using System.IO;
+using System.Drawing;
+using Cosmos.Core;
+using AmogusOS.System;
+using AmogusOS.System.Converting;
+
+namespace AmogusOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            FS fs = new FS();
+            fs.InitFS();
+            Setup stp = new Setup();
+            stp.CheckForInstalled();
+        }
+
+        protected override void Run()
+        {
+
+        }
+    }
+}
+>>>>>>> 94899b1 (Adding initial files.)
